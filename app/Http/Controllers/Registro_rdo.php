f2ac86fa9c1f31e66e028d7f7a5916cfd308c74b
vianawill <willian.v.f.wv@gmail.com>
2024-10-30 21:56:41 -0300

    Inserção dos campos de preenchimento

--- app/Http/Controllers/Registro_rdo.php
+++ app/Http/Controllers/Registro_rdo.php
@@ -14,7 +14,7 @@ class Registro_rdo extends Controller
      */
     public function index()
     {
-        return view ('layouts.gerarRDO');
+        return view ('gerarRDO');
     }
 
     /**
---
+++ resources/views/gerarRDO.blade.php
@@ -0,0 +1,110 @@
+@extends('adminlte::page')
+@section('content')
+<h1>Gerar RDO</h1>
+<form>
+    <div>
+        <h3 class="md-6">Cliente</h3>
+    </div>
+    <div class="form-row">
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+</form>
+
+<form>
+    <div>
+        <h3 class="md-6">Cliente</h3>
+    </div>
+    <div class="form-row">
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+
+    <div class="form-group col-6 md-6">
+        <label for="inputnome1">Nome</label>
+        <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+    </div>
+</form>
+
+<form>
+    <div>
+        <h3 class="md-6">Cliente</h3>
+    </div>
+    <div class="form-row">
+        <div class="form-group col-6 md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group col-8 md-4">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+</form>
+
+<form>
+    <div>
+        <h3 class="md-6">Cliente</h3>
+    </div>
+    <div class="form-row">
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+    </div>
+</form>
+
+<form>
+    <div>
+        <h3 class="md-6">Cliente</h3>
+    </div>
+    <div class="form-row">
+        <div class="form-group col md-6">
+            <label for="inputnome1">Nome</label>
+            <input type="text" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+        </div>
+        
+    </div>
+</form>
+@stop
