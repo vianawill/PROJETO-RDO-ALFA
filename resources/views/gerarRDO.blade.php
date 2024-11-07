ba6f4ccfdd216e515d2e457ae5299f5e6c863b27
vianawill <willian.v.f.wv@gmail.com>
2024-11-06 21:55:11 -0300

    criaçãocao de migrate e alteracao de nomenclatura de input

--- resources/views/gerarRDO.blade.php
+++ resources/views/gerarRDO.blade.php
@@ -1,6 +1,8 @@
 @extends('adminlte::page')
 @section('content')
-<h1>Gerar RDO</h1>
+<head>
+    <h1>Gerar RDO</h1>
+</head>
 <form>
     <div>
         <h3 class="md-6">Cliente</h3>
@@ -8,22 +10,22 @@
     <div class="form-row">
         <div class="form-group col md-6">
             <label for="inputnome1">Nome</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Nome">
+            <input type="text" class="form-control" id="inputnome1" name="cliente_nome" placeholder="Digite seu nome completo...">
         </div>
         <div class="form-group col md-6">
-            <label for="inputnome1">Razão Social</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Razão Social">
+            <label for="inputrazao">Razão Social</label>
+            <input type="text" class="form-control" id="inputrazao" name="cliente_razao" placeholder="Digite a razão social...">
         </div>
     </div>
 
     <div class="row">
         <div class="form-group col md-6">
-            <label for="inputnome1">Email</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Email">
+            <label for="inputemail">Email</label>
+            <input type="text" class="form-control" id="inputemail" name="cliente_email" placeholder="exemplo@dominio.com">
         </div>
         <div class="form-group col md-6">
-            <label for="inputnome1">Telefone</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Telefone">
+            <label for="inputtelefone">Telefone</label>
+            <input type="text" class="form-control" id="inputtelefone" name="cliente_telefone" placeholder="(XX) XXXXX-XXXX">
         </div>
     </div>
 </form>
@@ -34,29 +36,29 @@
     </div>
     <div class="form-row">
         <div class="form-group col md-6">
-            <label for="inputnome1">Horário Planejado</label>
-            <input type="time" class="form-control" id="inputnome1" placeholder="Horario Planejado">
+            <label for="inputhorarioplanejado">Horário Planejado</label>
+            <input type="text" class="form-control" id="inputhorarioplanejado" name="servico_horario_planejado" placeholder="Informe a data e o horario planejado...">
         </div>
         <div class="form-group col md-6">
-            <label for="inputnome1">Horário Realizado</label>
-            <input type="time" class="form-control" id="inputnome1" placeholder="Horário Realizado">
+            <label for="inputhorariorealizado">Horário Realizado</label>
+            <input type="text" class="form-control" id="inputhorariorealizado" name="servico_horario_realizado" placeholder="Informe a data e o horario realizado...">
         </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col md-6">
-            <label for="inputnome1">Tempo de Execução</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Tempo de Execução">
+            <label for="inputtempoexecucao">Tempo de Execução</label>
+            <input type="text" class="form-control" id="inputtempoexecucao" name="servico_tempo_execucao" placeholder="Tempo de Execução">
         </div>
         <div class="form-group col md-6">
-            <label for="inputnome1">Tempo de Início de Atendimento</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Tempo de Início de Atendimento">
+            <label for="inputtempoinicioatendimento">Tempo de Início do Atendimento</label>
+            <input type="text" class="form-control" id="inputtempoinicioatendimento" name="servico_tempo_inicio_atendimento" placeholder="Tempo do Início de Atendimento">
         </div>
     </div>
 
     <div class="form-group col-6 md-6">
-        <label for="inputnome1">Tempo de Finalizção do Atendimento</label>
-        <input type="text" class="form-control" id="inputnome1" placeholder="Tempo de Finalizção do Atendimento">
+        <label for="inputtempofinalizacaoatendimento">Tempo de Finalizção do Atendimento</label>
+        <input type="text" class="form-control" id="inputtempofinalizacaoatendimento" name="servico_tempo_finalizacao_atendimento" placeholder="Tempo de Finalizção do Atendimento">
     </div>
 </form>
 
@@ -66,15 +68,15 @@
     </div>
     <div class="form-row">
         <div class="form-group col-6 md-6">
-            <label for="inputnome1">Horário de Início</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Início do Trabalho">
+            <label for="inputhorarioinicio">Horário de Início</label>
+            <input type="text" class="form-control" id="inputhorarioinicio" name="servico_horario_inicio" placeholder="Informe a data e o horario do inicio do trabalho...">
         </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col-8 md-4">
-            <label for="inputnome1">Localização</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Localização">
+            <label for="inputlocalizacao">Localização</label>
+            <input type="text" class="form-control" id="inputlocalizacao" name="servico_localizacao" placeholder="Informe a sua localização...">
         </div>
     </div>
 </form>
@@ -83,14 +85,17 @@
     <div>
         <h3 class="md-6">Informações de Obra</h3>
     </div>
-    <div class="form-row">
+    <div class="form-group col md-6">
+        <label for="selectturno">Turno</label>
+        <select class="form-control" id="selectturno" name="servico_turno" placeholder="Informe o turno...">>
+        <option>Manhã</option>
+        <option>Tarde</option>
+        <option>Noite</option>
+        </select>
+    </div>
         <div class="form-group col md-6">
-            <label for="inputnome1">Turno</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Turno">
-        </div>
-        <div class="form-group col md-6">
-            <label for="inputnome1">Condição Climática</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Condição Climática">
+            <label for="inputcondicaoclimatica">Condição Climática</label>
+            <input type="text" class="form-control" id="inputcondicaoclimatica" name="servico_condicao_climatica" placeholder="Informe a condição climática...">
         </div>
     </div>
 </form>
@@ -101,15 +106,15 @@
     </div>
     <div class="form-row">
         <div class="form-group col-6 md-6">
-            <label for="inputnome1">Condições do Trabalho no Dia</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Condições do Trabalho no Dia">
+            <label for="inputcondicaotrabalho">Condições do Trabalho no Dia</label>
+            <input type="text" class="form-control" id="inputcondicaotrabalho" name="trabalho_co" placeholder="Informe as condições do trabalho no dia...">
         </div>
     </div>
     <div class="form-row">
         <div class="col-10">
             <label for="inputnome1" class="form-label">Limitador de Desempenho</label>
             <div class="input-group">
-                <input type="text" class="form-control" id="inputnome1" placeholder="Limitador de Desempenho">
+                <input type="text" class="form-control" id="inputnome1" placeholder="Informe as limitações de desempenho...">
             </div>
         </div>
     </div>
@@ -121,11 +126,11 @@
     <div class="form-row">
         <div class="form-group col-6 md-6">
             <label for="inputnome1">Código-Nome</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Código-Nome">
+            <input type="text" class="form-control" id="inputnome1" placeholder="Digite o código ou nome...">
         </div>
         <div class="form-group col-2 md-6">
             <label for="inputnome1">Quantidade</label>
-            <input type="number" class="form-control" id="inputnome1" placeholder="Quantidade">
+            <input type="number" class="form-control" id="inputnome1" placeholder="0">
         </div>
         <div class="form-group col-2 md-6">
             <label for="inputnome1">Preço Unt.</label>
@@ -145,11 +150,11 @@
     <div class="form-row">
         <div class="form-group col-6 md-6">
             <label for="inputnome1">Código-Nome</label>
-            <input type="text" class="form-control" id="inputnome1" placeholder="Código-Nome">
+            <input type="text" class="form-control" id="inputnome1" placeholder="Digite o código ou nome...">
         </div>
         <div class="form-group col-2 md-6">
             <label for="inputnome1">Quantidade</label>
-            <input type="number" class="form-control" id="inputnome1" placeholder="Quantidade">
+            <input type="number" class="form-control" id="inputnome1" placeholder="0">
         </div>
         <div class="form-group col-2 md-6">
             <label for="inputnome1">Preço Unt.</label>
